d953368009f5b22349e54b5a2cd3eb01ed01ed5e
Scaremonger <24841922+Scaremonger@users.noreply.github.com>
2020-07-12 10:43:37 +0100

    2020-07-12

---
+++ setup.py
@@ -0,0 +1,66 @@
+## Modular Home Automation - Core Modules
+## (c) Copyright Si Dunford, 2019
+
+import setuptools
+from setuptools.command.install import install
+
+VERSION_NUMBER = '1.0.3.1'
+VERSION_STATUS = 'Development Status :: 2 - Pre-Alpha'
+
+#Development Status :: 1 - Planning
+#Development Status :: 2 - Pre-Alpha
+#Development Status :: 3 - Alpha
+#Development Status :: 4 - Beta
+#Development Status :: 5 - Production/Stable
+#Development Status :: 6 - Mature
+#Development Status :: 7 - Inactive  
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+       
+class CustomInstaller(install):
+    def run(self):
+        import os,sys
+        
+        print( "Installing MIOT core modules" )
+        # Pre-Install
+        
+        # Install
+        install.run(self)
+        
+        # Post-Install
+        print( "- Platform: "+sys.platform )
+        #if sys.platform in ['darwin', 'linux']:
+        #    os.system('./linux_installer.sh')
+
+setuptools.setup(
+    name='miot',  
+    version=VERSION_NUMBER,
+    author="Si Dunford",
+    author_email="dunford.sj+miot@gmail.com",
+    description="Modular Internet of Things, Core modules",
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/automation-itspeedway-net/miotcore.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        VERSION_STATUS,
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Other Audience",
+        "Topic :: Scientific/Engineering :: Human Machine Interfaces"
+        ],
+    install_requires=[
+        'paho-mqtt',
+        'configparser'
+        ],
+    include_package_data=True,
+    python_requires='>=3.6',
+    scripts=['bin/miotrepl'],
+    cmdclass={
+        'install':CustomInstaller
+        }
+)
+
---
+++ test/test-config.py
@@ -0,0 +1,27 @@
+# Test the config module
+# (c) Copyright Si Dunford, Jul 2020
+
+from miot import config
+
+print( config.tostring() )
+
+# TEST CONFIG
+print( "RESULT="+config.get( "mqtt.host", "xyz" ) )
+print( "RESULT="+config.get( "mqtt.default.host", "abc" ) )
+print( "RESULT="+config.get( "rss.feed", "123" ) )
+
+print( config.tostring() )
+
+config.wipe()
+config.default()
+
+config.set( "mqtt.host", "192.168.1.228" )
+config.set( "mqtt.default.host", "192.168.1.253" )
+config.set( "rss.itspeedway", {'url':"https://itspeedway.net"} )
+config.set( "rss.bbc", {'url':"https://bbc.co.uk"} )
+
+config.save()
+
+print( config.tostring() )
+
+
